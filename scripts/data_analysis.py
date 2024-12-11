6dbac0924dfbabb42ef3e16bf4a2ab48672eab07
Ali.Turkarslan <aliturkarslan1994@gmail.com>
2024-12-11 18:55:31 +0200

    Update data_analysis.py

--- scripts/data_analysis.py
+++ scripts/data_analysis.py
@@ -1,4 +1,3 @@
-
 # Import libraries
 import pandas as pd
 import numpy as np
@@ -6,7 +5,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 # Load dataset
-file_path = "data/raw/Hi-small.csv"
+file_path = "data/raw/HI-small.csv"
 data = pd.read_csv(file_path)
 
 # Perform basic EDA
